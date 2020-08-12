78304547d950e208eaa7ca2e874b84bbf8b761cf
Kaishiyoku <dmp@nfze.de>
2020-08-12 12:14:54 +0200

    #4 added validator for CSS color names

--- src/Kaishiyoku/Validation/Color/Validator.php
+++ src/Kaishiyoku/Validation/Color/Validator.php
@@ -2,15 +2,181 @@
 
 namespace Kaishiyoku\Validation\Color;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Validator as BaseValidator;
 
 class Validator extends BaseValidator
 {
-    public const VALIDATION_NAMES = [
+    /**
+     * Available validation rule names
+     */
+    public const VALIDATION_RULE_NAMES = [
         'color',
         'color_hex',
         'color_rgb',
         'color_rgba',
+        'color_name',
+    ];
+
+    /**
+     * Supported color names
+     *
+     * @var array
+     */
+    public const AVAILABLE_COLOR_NAMES = [
+        // CSS Level 1
+        'silver',
+        'gray',
+        'white',
+        'maroon',
+        'red',
+        'purple',
+        'fuchsia',
+        'green',
+        'lime',
+        'olive',
+        'yellow',
+        'navy',
+        'blue',
+        'teal',
+        'aqua',
+
+        // CSS Level 2 (Revision 1)
+        'orange',
+        'aliceblue',
+        'antiquewhite',
+        'aquamarine',
+        'azure',
+        'beige',
+        'bisque',
+        'blanchedalmond',
+        'blueviolet',
+        'brown',
+        'burlywood',
+        'cadetblue',
+        'chartreuse',
+        'chocolate',
+        'coral',
+        'cornflowerblue',
+        'cornsilk',
+        'crimson',
+        'darkblue',
+        'darkcyan',
+        'darkgoldenrod',
+        'darkgray',
+        'darkgreen',
+        'darkgrey',
+        'darkkhaki',
+        'darkmagenta',
+        'darkolivegreen',
+        'darkorange',
+        'darkorchid',
+        'darkred',
+        'darksalmon',
+        'darkseagreen',
+        'darkslateblue',
+        'darkslategray',
+        'darkslategrey',
+        'darkturquoise',
+        'darkviolet',
+        'deeppink',
+        'deepskyblue',
+        'dimgray',
+        'dimgrey',
+        'dodgerblue',
+        'firebrick',
+        'floralwhite',
+        'forestgreen',
+        'gainsboro',
+        'ghostwhite',
+        'gold',
+        'goldenrod',
+        'greenyellow',
+        'grey',
+        'honeydew',
+        'hotpink',
+        'indianred',
+        'indigo',
+        'ivory',
+        'khaki',
+        'lavender',
+        'lavenderblush',
+        'lawngreen',
+        'lemonchiffon',
+        'lightblue',
+        'lightcoral',
+        'lightcyan',
+        'lightgoldenrodyellow',
+
+        // CSS Color Module Level 3
+        'lightgray',
+        'lightgreen',
+        'lightgrey',
+        'lightpink',
+        'lightsalmon',
+        'lightseagreen',
+        'lightskyblue',
+        'lightslategray',
+        'lightslategrey',
+        'lightsteelblue',
+        'lightyellow',
+        'limegreen',
+        'linen',
+        'mediumaquamarine',
+        'mediumblue',
+        'mediumorchid',
+        'mediumpurple',
+        'mediumseagreen',
+        'mediumslateblue',
+        'mediumspringgreen',
+        'mediumturquoise',
+        'mediumvioletred',
+        'midnightblue',
+        'mintcream',
+        'mistyrose',
+        'moccasin',
+        'navajowhite',
+        'oldlace',
+        'olivedrab',
+        'orangered',
+        'orchid',
+        'palegoldenrod',
+        'palegreen',
+        'paleturquoise',
+        'palevioletred',
+        'papayawhip',
+        'peachpuff',
+        'peru',
+        'pink',
+        'plum',
+        'powderblue',
+        'rosybrown',
+        'royalblue',
+        'saddlebrown',
+        'salmon',
+        'sandybrown',
+        'seagreen',
+        'seashell',
+        'sienna',
+        'skyblue',
+        'slateblue',
+        'slategray',
+        'slategrey',
+        'snow',
+        'springgreen',
+        'steelblue',
+        'tan',
+        'thistle',
+        'tomato',
+        'turquoise',
+        'violet',
+        'wheat',
+        'whitesmoke',
+        'yellowgreen',
+
+        // CSS Color Module Level 4
+        'rebeccapurple',
     ];
 
     /**
@@ -22,7 +188,7 @@ class Validator extends BaseValidator
      */
     public function validateColor($attribute, $value)
     {
-        $fnNames = ['validateColorHex', 'validateColorRGB', 'validateColorRGBA'];
+        $fnNames = ['validateColorHex', 'validateColorRGB', 'validateColorRGBA', 'validateColorName'];
 
         return $this->checkValidationFnsFor($fnNames, $attribute, $value);
     }
@@ -67,6 +233,18 @@ class Validator extends BaseValidator
         $regex = '/^(rgba)\(([01]?\d\d?|2[0-4]\d|25[0-5])\W+([01]?\d\d?|2[0-4]\d|25[0-5])\W+([01]?\d\d?|2[0-4]\d|25[0-5])\)?\W+([01](\.\d+)?)\)$/i';
 
         return $this->callPregMatcher($value, $regex);
+    }
+
+    /**
+     * The color name validator
+     *
+     * @param string $attribute
+     * @param string $value
+     * @return bool
+     */
+    public function validateColorName($attribute, $value)
+    {
+        return in_array($value, self::AVAILABLE_COLOR_NAMES, true);
     }
 
     /**
